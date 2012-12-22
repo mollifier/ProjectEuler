8f66f7564936681273b28bb8e2d23f7637cbdc21
mollifier <mollifier@gmail.com>
2012-12-22 15:38:53 +0900

    isPrime

--- problem2.scala
+++ problem2.scala
@@ -1,4 +1,5 @@
 
+// 素数なら true
 def isPrime(n: Int): Boolean = {
   if (n == 1) {
     return false
@@ -7,12 +8,16 @@ def isPrime(n: Int): Boolean = {
   // 2 から n 未満の値ですべて割り切れない場合は true
   (2 until n).filter(x => n % x == 0).length == 0
 }
+// テスト
+//println((1 to 10).map(x => x.toString + " " + isPrime(x).toString))
 
+// n の素因数を返す
 def soinsu(n: Int): List[Int] = {
-  List(1, 2, 3)
+  (2 to n).filter(x => isPrime(x)).filter(x => n % x == 0)
 }
+// テスト
+println(soinsu(10))
 
 val m = soinsu(10).max
 
-println((1 to 10).map(x => x.toString + " " + isPrime(x).toString))
 
