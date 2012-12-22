9f1a2de73f76c86c08fea372d9460c233015a672
mollifier <mollifier@gmail.com>
2012-12-22 16:20:35 +0900

    素因数分解

--- problem2.scala
+++ problem2.scala
@@ -1,29 +1,50 @@
 
 // 素数なら true
-def isPrime(n: Int): Boolean = {
+def isPrime(n: Long): Boolean = {
+//def isPrime(n: Int): Boolean = {
   if (n == 1) {
     return false
   }
 
   // 2 から n 未満の値ですべて割り切れない場合は true
-  (2 until n).filter(x => n % x == 0).length == 0
+  // 2 から n 未満の値で一つでも割り切れた場合は false
+
+  var i = 2
+  while (i < n) {
+    if (n % i == 0) {
+      return false
+    }
+
+    i += 1
+  }
+
+  return true
 }
 // テスト
-//println((1 to 10).map(x => x.toString + " " + isPrime(x).toString))
+//println((1 to 20).map(x => x.toString + " " + isPrime(x).toString))
+//println((1L to 600851475143L).map(x => x.toString + " " + isPrime(x).toString))
 
-// n の素因数を返す
-//def soinsu(n: Int): List[Int] = {
-def soinsu(n: Int) = {
-  (2 to n).filter(x => isPrime(x)).filter(x => n % x == 0)
-  //(2 to n).filter(x => isPrime(x))
-  //(2 to n)
+
+// n の最大の素因数を返す
+def maxSoinsu(n: Long): Long = {
+
+  var m = 1
+
+  var i = 2
+  while (i <= n) {
+    if (n % i == 0 && isPrime(i)) {
+      m = i
+    }
+
+    i += 1
+  }
+
+  m
 }
-// テスト
-//println(soinsu(14))
-//println(soinsu(13195))
 
-val m = soinsu(13195).max
+//val m = maxSoinsu(34)
+//val m = maxSoinsu(13195)
+val m = maxSoinsu(600851475143L)
 
 println(m)
-
 
