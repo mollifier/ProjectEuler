215d7ca9ab6032201f2499c91ea3a805377b64b7
mollifier <mollifier@gmail.com>
2012-12-22 17:22:01 +0900

    追加

--- problem4.scala
+++ problem4.scala
@@ -15,8 +15,19 @@ def isKaibun(n: Int): Boolean = {
 //println(isKaibun(123))
 //println(isKaibun(123321))
 
-val m = (10 to 99).map(x => {(10 to 99).map(y => x * y)} ).flatten.filter(x => isKaibun(x)).max
-//val m = (10 to 99).map(x => {(10 to 99).map(y => x * y)}  ).filter(x => isKaibun(x)).max
+//val m = (10 to 99).map(x => {(10 to 99).map(y => x * y)} ).flatten.filter(x => isKaibun(x)).max
+//val m = (100 to 999).map(x => {(100 to 999).map(y => x * y)} ).filter(x => isKaibun(x)).max
+
+var m = 0
+for (x <- (100 to 999)) {
+  var tmpList = (100 to 999).map(y => x * y).filter(z => isKaibun(z))
+  if (tmpList.length > 0) {
+    var tmpMax = tmpList.max
+    if (tmpMax > m) {
+      m = tmpMax
+    }
+  }
+}
 
 println(m)
 
